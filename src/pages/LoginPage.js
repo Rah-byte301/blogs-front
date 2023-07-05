c28af4bbb338ac5e1550becdf1dd050d300a6d53
Rahul Ganguly <63881386+Rah-byte301@users.noreply.github.com>
2023-07-06 03:57:28 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,7 +9,7 @@ const LoginPage = () => {
   const { setUserInfo } = useContext(UserContext);
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch("http://localhost:4000/login", {
+    const response = await fetch("https://blogend.onrender.com/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },

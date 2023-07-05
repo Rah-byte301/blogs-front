17c9d25e60dfd381b39c66f4f6549b04c65c2d05
Rahul Ganguly <63881386+Rah-byte301@users.noreply.github.com>
2023-07-06 03:27:40 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import { UserContext } from "./UserContext";
 const Header = () => {
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch("https://localhost:4000/profile", {
+    fetch("https://blogend.onrender.com/profile", {
       credentials: "include",
     }).then((response) => {
       response.json().then((userInfo) => {
@@ -15,7 +15,7 @@ const Header = () => {
   }, []);
 
   function logout() {
-    fetch("http://localhost:4000/logout", {
+    fetch("https://blogend.onrender.com/logout", {
       credentials: "include",
       method: "POST",
     });
